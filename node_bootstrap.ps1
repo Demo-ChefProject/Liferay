3c8d8e8801a7e0c68ff1d44d1be98b38dfbc2a2b
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-30 16:09:27 -0400

    commit

---
+++ node_bootstrap.ps1
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#cd ~/home/jenkins/workspace/Chef-apache/
+#cd C:/Users/krithi.gananasekaran/workspace/Chef-Jdk
+cd C:\Jenkins\workspace\Liferay\
+
+#for jdk
+#sudo knife role from file /home/jenkins/workspace/Chef-apache/roles/apache.rb
+#knife role from file C:\Users\krithi.gnanasekaran\workspace\Chef-Jdk\roles\jdkinstall.rb
+knife role from file C:\Jenkins\workspace\Liferay\roles\liferay.json
+
+
+# Upload cookbooks into Chef Server
+knife upload cookbooks liferay
+
+
+# Bootstrap a node to its chef server
+knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[liferay]' -y
+
+#Passing credentials stored as a secure string
+$Pass = cat C:\securestring.txt | convertto-securestring
+$Pegacred = new-object -TypeName System.Management.Automation.PSCredential -argumentlist "Administrator",$pass
+#  Create a remote session to the chef node
+$Session = New-PSSession -ComputerName 54.175.57.21 -Credential $Pegacred
+
+$Script = {powershell.exe chef-client}
+
+$Job = Invoke-Command -Session $Session -Scriptblock $Script
+echo $Job
+#Script which runs the ruby script in the remote server
+
+
+# Exit and remove the current session
+Remove-PSSession -Session $Session
