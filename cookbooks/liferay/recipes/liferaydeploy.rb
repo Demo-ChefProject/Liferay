f2b58f9194760b6294792cc881acc98ce35bbaa9
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-31 17:18:12 -0400

    Update liferaydeploy.rb

--- cookbooks/liferay/recipes/liferaydeploy.rb
+++ cookbooks/liferay/recipes/liferaydeploy.rb
@@ -17,10 +17,10 @@ end
 powershell_script 'Remove logs' do
   guard_interpreter :powershell_script
   code <<-EOH
-    Remove-Item C:NC4/MC3/logs/* -recurse
+    Remove-Item C:\liferay\MC3\logs\* -recurse
   EOH
   #only_if do Dir.exist? C:NC4/MC3/logs
-  notifies :run, 'powershell_script[Remove log,error,temp in tomcat]', :immediately
+  #notifies :run, 'powershell_script[Remove log,error,temp in tomcat]', :immediately
 end
 =begin
 powershell_script 'Remove log,error,temp in tomcat' do
