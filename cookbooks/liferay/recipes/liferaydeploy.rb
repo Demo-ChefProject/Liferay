273f06937c62a034875f4c92d18ddf40b5ef6bd1
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-01 09:33:12 -0400

    Update liferaydeploy.rb

--- cookbooks/liferay/recipes/liferaydeploy.rb
+++ cookbooks/liferay/recipes/liferaydeploy.rb
@@ -9,11 +9,11 @@ end
 powershell_script 'Unzip Liferay package' do
   guard_interpreter :powershell_script
   code <<-EOH
-    powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('C:\liferay\liferay-base-install-6.1.30.zip', 'C:\liferay')}"
+    powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('C:\liferay\liferay-base-install-6.1.30.zip', 'C:\liferay\')}"
   EOH
-  notifies :run, 'powershell_script[Remove logs]', :immediately
+  #notifies :run, 'powershell_script[Remove logs]', :immediately
 end
-
+=begin
 powershell_script 'Remove logs' do
   guard_interpreter :powershell_script
   code <<-EOH
@@ -22,7 +22,7 @@ powershell_script 'Remove logs' do
   #only_if do Dir.exist? C:NC4/MC3/logs
   #notifies :run, 'powershell_script[Remove log,error,temp in tomcat]', :immediately
 end
-=begin
+
 powershell_script 'Remove log,error,temp in tomcat' do
   guard_interpreter :powershell_script
   code <<-EOH
