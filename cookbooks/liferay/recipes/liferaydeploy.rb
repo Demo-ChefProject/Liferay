50e10856a6d44e497c9c49c0ffedae59b6c41a28
sananda1 <skumar@agiletrailblazers.com>
2016-09-02 10:30:56 -0400

    updating LR

--- cookbooks/liferay/recipes/liferaydeploy.rb
+++ cookbooks/liferay/recipes/liferaydeploy.rb
@@ -17,9 +17,9 @@ end
 remote_file "#{liferay_install_loc}/#{liferay_package_name}" do
   source 'http://ec2-54-175-158-124.compute-1.amazonaws.com/repository/Rigil/liferay-base-install-6.1.30.zip'
   action :create
-  notifies :run, 'powershell_script[Unzip Liferay package]', :immediately
+  notifies :run, 'powershell_script[backup current install]', :immediately
 end
-=begin
+
 #Backup the current install
 powershell_script 'backup current install' do
   guard_interpreter :powershell_script
@@ -29,11 +29,11 @@ powershell_script 'backup current install' do
   not_if do Dir.exist?("#{liferay_work_dir}-#{liferay_backup_touch}") end
   notifies :run, 'powershell_script[Unzip Liferay package]', :immediately
 end
-=end
+
 powershell_script 'Unzip Liferay package' do
   guard_interpreter :powershell_script
   code <<-EOH
-   powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('#{liferay_install_loc}/#{liferay_package_name}', '#{liferay_install_loc}');}"
+   powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('#{liferay_install_loc}/#{liferay_package_name}', '#{liferay_install_loc}'); }"
  EOH
   notifies :run, 'powershell_script[Remove logs]', :immediately
 end
