579618e0f989bd5198189c9c853f0507cedf2afa
sananda1 <skumar@agiletrailblazers.com>
2016-09-02 10:39:56 -0400

    fixing LR

--- cookbooks/liferay/recipes/liferaydeploy.rb
+++ cookbooks/liferay/recipes/liferaydeploy.rb
@@ -33,7 +33,7 @@ end
 powershell_script 'Unzip Liferay package' do
   guard_interpreter :powershell_script
   code <<-EOH
-   powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('#{liferay_install_loc}/#{liferay_package_name}', '#{liferay_install_loc}'); }"
+    powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('#{liferay_install_loc}/#{liferay_package_name}', '#{liferay_install_loc}'); }"
  EOH
   notifies :run, 'powershell_script[Remove logs]', :immediately
 end
