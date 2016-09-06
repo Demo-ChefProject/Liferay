5f55c911e963adeaff5e6745e86eb0e190bd1f06
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-06 09:41:53 -0400

    Update default.rb

--- cookbooks/liferay/attributes/default.rb
+++ cookbooks/liferay/attributes/default.rb
@@ -1,2 +1,4 @@
 default['nc4']['liferay']['install_location'] = "C:/NC4"
 default['nc4']['liferay']['package']="liferay-base-install-6.1.30.zip"
+default['nc4']['nexus']['url']="http://54.175.158.124:8081"
+default['nc4']['liferay1']['version']="repository/Rigil"
