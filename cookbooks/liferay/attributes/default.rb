979c1e173c33d1eb3e445ef9e16799d52219b811
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-06 09:00:43 -0400

    Update default.rb

--- cookbooks/liferay/attributes/default.rb
+++ cookbooks/liferay/attributes/default.rb
@@ -1,2 +1,2 @@
-default['nc4']['liferay']['install_location'] = "C:/NC4"
+default['nc4']['liferay']['install_location'] = "C:/NC4/liferay"
 default['nc4']['liferay']['package']="liferay-base-install-6.1.30.zip"
