2a71f94232697ecf94e59d37f274b4aa96690fd5
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-15 16:11:36 -0400

    adding property file

--- cookbooks/liferay/recipes/installtomcat.rb
+++ cookbooks/liferay/recipes/installtomcat.rb
@@ -2,5 +2,21 @@
 
 liferay_work_dir = "#{liferay_install_loc}/MC3"
 liferay_portal_dir = "#{liferay_install_loc}/MC3/portal-exe.propertiestomcat"
+liferay_max_size = node['nc4']['max_size']
 
+=begin
+template "#{apache_httpd_conf}/httpd-vhost.conf" do
+  source 'httpd-vhosts.conf.erb'
+  variables( :server_name => apache_server_name )
+  action :create
+end
+=end
 
+template "liferay_portal_dir" do
+  source 'portal-ext.properties.erb'
+  variables({
+    :max_size => liferay_max_size,
+  #  :work_dir => apache_work_dir
+    })
+  action :create
+end
