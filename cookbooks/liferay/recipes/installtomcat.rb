3d9ef4cd19012834d5eb3b5c1dec0c6b034f9d43
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-16 10:44:17 -0400

    Update installtomcat.rb

--- cookbooks/liferay/recipes/installtomcat.rb
+++ cookbooks/liferay/recipes/installtomcat.rb
@@ -6,6 +6,8 @@ liferay_work_dir = "#{liferay_install_loc}/MC3"
 liferay_max_size = node['nc4']['max_size']
 liferay_ip_home = node['nc4']['ip_home']
 liferay_ip_internal = node['nc4']['ip_internal']
+liferay_session_timeout = node['nc4']['session_timeout']
+
 
 =begin
 template "#{apache_httpd_conf}/httpd-vhost.conf" do
@@ -21,6 +23,7 @@ template "#{liferay_work_dir}/portal-exe.properties" do
     :max_size => liferay_max_size,
     :ip_home => liferay_ip_home,
     :ip_internal => liferay_ip_internal,
+    :session_timeout => liferay_session_timeout,
     :work_dir => liferay_work_dir
     })
   action :create
