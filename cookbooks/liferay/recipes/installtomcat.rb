8afa6c1dc88a77814ca82e9719cc9a328a08cf49
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-16 10:28:27 -0400

    Update installtomcat.rb

--- cookbooks/liferay/recipes/installtomcat.rb
+++ cookbooks/liferay/recipes/installtomcat.rb
@@ -1,8 +1,11 @@
 #Changes made in portal-exe.properties
 
-#liferay_install_loc = node['nc4']['liferay']['install_location']
-#liferay_work_dir = "#{liferay_install_loc}/MC3"
-#liferay_max_size = node['nc4']['max_size']
+liferay_install_loc = node['nc4']['liferay']['install_location']
+liferay_work_dir = "#{liferay_install_loc}/MC3"
+
+liferay_max_size = node['nc4']['max_size']
+liferay_ip_home = node['nc4']['ip_home']
+liferay_ip_internal = node['nc4']['ip_internal']
 
 =begin
 template "#{apache_httpd_conf}/httpd-vhost.conf" do
@@ -11,13 +14,15 @@ template "#{apache_httpd_conf}/httpd-vhost.conf" do
   action :create
 end
 =end
-=begin
+
 template "#{liferay_work_dir}/portal-exe.properties" do
   source 'portal-ext.properties.erb'
   variables({
     :max_size => liferay_max_size,
+    :ip_home => liferay_ip_home,
+    :ip_internal => liferay_ip_internal,
     :work_dir => liferay_work_dir
     })
   action :create
 end
-=end
+
