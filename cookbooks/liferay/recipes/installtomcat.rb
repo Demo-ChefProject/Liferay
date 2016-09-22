29e0d0566de8bc8c3011e0e05b547192f7477fa9
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-22 15:14:20 -0400

    Update installtomcat.rb

--- cookbooks/liferay/recipes/installtomcat.rb
+++ cookbooks/liferay/recipes/installtomcat.rb
@@ -82,11 +82,12 @@ Compare-Object filepath$ line1$
 
 If ($filepath -match "24fsf24")
 {
+echo "Yippee, pswd comp exists"
 Write-Host "Yippee, pswd comp exists"
 }
 ELSE {
 add-content C:\NC4\MC3\portal-ext.properties "<p><i>passwords.passwordpolicytoolkit.charset.lowercase=abcdefghijklmnopqrstuvwxyz</i>"
-Write-Host "passwords.passwordpolicytoolkit.charset.lowercase=abcdefghijklmnopqrstuvwxyz"
+echo "passwords.passwordpolicytoolkit.charset.lowercase=abcdefghijklmnopqrstuvwxyz"
 }
 EOH
  #notifies :run, 'execute[delete if tomcat service exist]', :immediately
