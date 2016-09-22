d825119be88001f8bf0e7b261ed1f5d0d0455aee
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-22 12:08:09 -0400

    Update installtomcat.rb

--- cookbooks/liferay/recipes/installtomcat.rb
+++ cookbooks/liferay/recipes/installtomcat.rb
@@ -114,9 +114,9 @@ end
 
 powershell_script 'install Tomcat Service' do
   code <<-EOH
-     $Service = Get-Service -Name Apache-Tomcat-Krithi1MC3 -ErrorAction SilentlyContinue
+     $Service = Get-Service -Name Apache-Tomcat-MC3 -ErrorAction SilentlyContinue
      if (! $Service) {
-           Invoke-Expression "cmd /c C:/NC4/MC3/tomcat/bin/service.bat install Krithi1MC3" 
+           Invoke-Expression "cmd /c C:/NC4/MC3/tomcat/bin/service.bat install MC3" 
      }
   EOH
 end
