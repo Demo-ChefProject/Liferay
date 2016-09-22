1e2828eda6d7fd09068e55e47c85822fb513f8dd
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-09-22 15:29:35 -0400

    Update installtomcat.rb

--- cookbooks/liferay/recipes/installtomcat.rb
+++ cookbooks/liferay/recipes/installtomcat.rb
@@ -86,7 +86,7 @@ echo "Yippee, pswd comp exists"
 Write-Host "Yippee, pswd comp exists"
 }
 ELSE {
-add-content C:\NC4\MC3\portal-ext.properties "<p><i>passwords.passwordpolicytoolkit.charset.lowercase=abcdefghijklmnopqrstuvwxyz</i>"
+add-content C:\\NC4\\MC3\\portal-ext.properties "'n passwords.passwordpolicytoolkit.charset.lowercase=abcdefghijklmnopqrstuvwxyz"
 echo "passwords.passwordpolicytoolkit.charset.lowercase=abcdefghijklmnopqrstuvwxyz"
 }
 EOH
